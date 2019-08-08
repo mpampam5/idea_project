637f57b51890287d93b96a7878da5aa8c018fdc1
mpampam5 <mpampam5@gmail.com>
2019-08-09 00:54:27 +0700

    benahiii tampilan member dashboard

--- app/modules/backend/controllers/Home.php
+++ app/modules/backend/controllers/Home.php
@@ -9,14 +9,18 @@ class Home extends MY_Controller{
   {
     parent::__construct();
     $this->load->model('Pohon_model','model');
+    $this->load->library(array("btree"));
   }
 
   function index()
   {
     $this->template->set_title('Home');
     $data['balance'] = $this->balance->total_balance(sess('id_member'));
+    $data['comission']  = $this->balance->sponsor(sess('id_member'));
     $data['deposit'] = $this->balance->deposit(sess('id_member'));
     $data['withdraw'] = $this->balance->withdraw(sess('id_member'));
+    $data['left_group'] = $this->btree->leftcount(sess('id_member'));
+    $data['right_group'] = $this->btree->rightcount(sess('id_member'));
     $this->template->view('index',$data);
   }
 
--- app/modules/backend/views/index.php
+++ app/modules/backend/views/index.php
@@ -1,24 +1,57 @@
+<style media="screen">
+  input[readonly]{
+
+  }
+</style>
+
+<div class="row">
+  <div class="col-md-12 grid-margin">
+    <div class="row">
+      <div class="col-12 col-xl-5 mb-4 mb-xl-0">
+        <h4 class="font-weight-bold">Hi, Welcomeback!</h4>
+        <h4 class="font-weight-normal mb-0"><?=profile("nama")?></h4>
+      </div>
+      <div class="col-12 col-xl-7">
+        <div class="d-flex align-items-center justify-content-between flex-wrap">
+          <div class="border-right pr-4 mb-3 mb-xl-0">
+            <p class="text-muted">Balance</p>
+            <h4 class="mb-0 font-weight-bold">Rp. <?=format_rupiah($balance)?></h4>
+          </div>
+          <div class="border-right pr-4 mb-3 mb-xl-0">
+            <p class="text-muted">Comission</p>
+            <h4 class="mb-0 font-weight-bold">Rp. <?=format_rupiah($comission)?></h4>
+          </div>
+          <div class="border-right pr-4 mb-3 mb-xl-0">
+            <p class="text-muted">Username</p>
+            <h4 class="mb-0 font-weight-bold"><?=profile('username')?></h4>
+          </div>
+          <div class="border-right pr-4 mb-3 mb-xl-0">
+            <p class="text-muted">Paket</p>
+            <h4 class="mb-0 font-weight-bold"><?=paket(profile('paket'),'paket')?></h4>
+          </div>
+          <div class="pr-3 mb-3 mb-xl-0">
+            <p class="text-muted">Status Stockis</p>
+            <h4 class="mb-0 font-weight-bold">
+              <?php if (profile("status_stockis")=="member"): ?>
+                Member
+                <?php else: ?>
+                  Master Stockis
+              <?php endif; ?>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 <div class="row mb-4">
-  <div class="col-lg-12">
-    <div class="card">
-      <div class="card-body">
-        <div class="row">
-          <div class="col-sm-5">
-            <div class="col-12 col-xl-5 mb-4 mb-xl-0">
-                  <h4 class="font-weight-bold">Hi, Welcome!</h4>
-                  <h4 class="font-weight-normal mb-0"><?=profile("nama")?></h4>
-            </div>
-          </div>
-          <div class="col-sm-7">
-            <div class="form-group">
-              <div class="input-group">
-                <input type="text" class="form-control" id="copy-referral" placeholder="Link Referral" aria-label="Link Referral" value="<?=site_url("referral/".profile("kode_referral"))?>" readonly>
-                <div class="input-group-append">
-                  <button class="btn btn-sm btn-primary btn-clipboard" type="button" data-clipboard-action="copy" data-clipboard-target="#copy-referral">Copy Link Referral</button>
-                </div>
-              </div>
-            </div>
-          </div>
+  <div class="col-sm-12">
+    <div class="form-group">
+      <div class="input-group">
+        <input type="text" class="form-control" id="copy-referral" placeholder="Link Referral" aria-label="Link Referral" value="<?=site_url("referral/".profile("kode_referral"))?>" readonly style="background: #fff;border:1px solid #f5ce00">
+        <div class="input-group-append">
+          <button class="btn btn-sm btn-warning btn-clipboard text-white" type="button" data-clipboard-action="copy" data-clipboard-target="#copy-referral">Copy Link Referral</button>
         </div>
       </div>
     </div>
@@ -27,7 +60,7 @@
 
 
 <div class="row">
-
+<!--
   <div class="offset-md-2 col-md-4 grid-margin stretch-card">
     <div class="card bg-primary text-white">
       <div class="card-body">
@@ -48,40 +81,9 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 
-  <div class="col-md-4 grid-margin stretch-card">
-    <div class="card bg-success text-white">
-      <div class="card-body">
-        <p class="card-title text-md-center text-xl-left text-white">Balance</p>
-        <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-          <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">Rp. <?=format_rupiah($balance)?></h3>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <div class="col-md-4 grid-margin stretch-card">
-    <div class="card bg-success text-white">
-      <div class="card-body">
-        <p class="card-title text-md-center text-xl-left text-white">Comission</p>
-        <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-          <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">Rp. <?=format_rupiah($this->balance->sponsor(sess('id_member')))?></h3>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-md-4 grid-margin stretch-card">
-    <div class="card bg-success text-white">
-      <div class="card-body">
-        <p class="card-title text-md-center text-xl-left text-white">Status Stockis</p>
-        <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-          <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">Gold</h3>
-        </div>
-      </div>
-    </div>
-  </div>
 
 
   <div class="col-md-4 grid-margin stretch-card">
@@ -100,7 +102,7 @@
       <div class="card-body">
         <p class="card-title text-md-center text-xl-left text-white">Group Left</p>
         <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-          <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">0 Orang</h3>
+          <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0"><?=$left_group?> Orang</h3>
         </div>
       </div>
     </div>
@@ -111,7 +113,7 @@
       <div class="card-body">
         <p class="card-title text-md-center text-xl-left text-white">Group Right</p>
         <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-          <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">0 Orang</h3>
+          <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0"><?=$right_group?> Orang</h3>
         </div>
       </div>
     </div>
